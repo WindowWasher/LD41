caab7a556815eab7902fa11dd8a0cc56c142c468
Aaron <aaronandrews29@gmail.com>
2018-04-23 04:50:45 -0600

    Fixed conflict

--- LD41/Assets/Scripts/Enemies/EnemyManager.cs
+++ LD41/Assets/Scripts/Enemies/EnemyManager.cs
@@ -78,7 +78,7 @@ public class EnemyManager : MonoBehaviour {
 
 
 
-        int numberOfEnemiesToSpawn = 5 + numPerWave * waveNumber;
+        int numberOfEnemiesToSpawn = 200* 5 + numPerWave * waveNumber;
         if(waveNumber == 0)
         {
             numberOfSpawnLocations = 2;
